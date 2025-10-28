58d411f6cef9a61c340e8de014896fef70136849
select <1164760+select@users.noreply.github.com>
2025-10-28 20:14:14 +0100

    Move tests to __tests__ and create interactive demo with index.html

--- __tests__/untgz.test.ts
+++ __tests__/untgz.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
-import { untgz } from './untgz';
-import { tarGetEntryData } from './libuntar.js';
+import { untgz } from '../untgz';
+import { tarGetEntryData } from '../libuntar.js';
 import { readFileSync } from 'fs';
 
 describe('untgz', () => {
